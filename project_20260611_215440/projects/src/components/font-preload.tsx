bb3e1701d2790d56b1c633de40c26980f28a9bde
XYT-28 <2630056829@qq.com>
2026-06-11 23:32:07 +0800

    Delete project_20260611_215440 directory

--- project_20260611_215440/projects/src/components/font-preload.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import ReactDOM from 'react-dom';
-
-export function FontPreload() {
-  ReactDOM.preconnect('https://fonts.googleapis.cn');
-  ReactDOM.preconnect('https://fonts.gstatic.cn');
-  return null;
-}
